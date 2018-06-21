ca951480b6d9f20cf9bbe33c6219d8307e87d830
dshiyanov92 <40457395+dshiyanov92@users.noreply.github.com>
2018-06-21 12:52:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BestCase bc = new BestCase();
+            RandomValue rand = new RandomValue();
+            rand.test1(); // Random values
+            bc.test2(); // Array sorted in ascending order
+
+
+        }
+        /*
+        StreamWriter str = new StreamWriter(fileName);
+        for (int i = 0; i < size; i++)
+        {
+            str.WriteLine(testArray[i]);
+        }
+        str.Close();
+        */
+    }
+}
